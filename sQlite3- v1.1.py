fb5eb80d9e2b10b84cb4d4922b7df1bd5c7526bc
Th3Et3rnalz <joe.verbist@gmail.com>
2018-11-20 22:30:24 +0100

    Added search feature

---
+++ sQlite3- v1.1.py
@@ -0,0 +1,119 @@
+import sqlite3
+
+columnWidth = 20
+columnName = 0
+
+def tableExists(tableName):
+	sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "'"
+	for row in c.execute(sql):
+		return ''.join(row)
+
+
+def lenI(integer):
+	count = 0
+	while (integer > 0):
+		integer = integer /10
+		count = count + 1
+	return count
+
+def readColumns(table):
+	columnsQuery = "PRAGMA table_info(%s)" % table
+	c.execute(columnsQuery)
+	numberOfColumns = len(c.fetchall())
+	return numberOfColumns
+
+def devMode():
+	sql = "SELECT name FROM sqlite_master WHERE type='table'"
+	for name in c.execute(sql):
+		print name
+def fixWidth(var, width):
+	if isinstance(var, float):
+        	var = int(var)
+        try:
+                if len(var) < width:
+			whitespace = " "*(width - len(var))
+                        var = var + whitespace
+        except:
+                if lenI(var) < width:
+			if lenI(var) == 0:
+				width = width - 1
+                        whitespace = " "*(width- lenI(var))
+                        var = str(var) + whitespace
+	return var
+
+try:
+	file = raw_input('What file do you want to open?: ')
+	conn = sqlite3.connect(file)
+	c = conn.cursor()
+except:
+	print ("Something went wrong. Connecting to the file and/or opening it didn't work.")
+
+try:
+	table = raw_input("what table do you want to use? ")
+	existence = tableExists(table)
+        if table == "DEV":
+                print "You have now entered developper mode"
+                a = devMode()
+		exit()
+	if existence != table:
+		print("This table does not exist")
+		build = raw_input("Do you want to create it?")
+		if build == "yes" or build == "y":
+			sql = "CREATE IF NOT EXISTS "+table + " (date text, trans, text, qty real, price real)"
+			c.execute(sql)
+		else:
+			exit()
+except:
+	print ("Error, couldn't open or create the table")
+try:
+	proceed = raw_input("What do you want to do? ")
+except:
+	print "Goodbye"
+	proceed = ""
+
+if proceed == "W" or proceed == "Write" or proceed == "write":
+	print (table)
+	date = raw_input("DATE : ")
+	trans = raw_input("TRANS: ")
+	qty = raw_input("QTY:   ")
+	price = raw_input("PRICE: ")
+	try:
+		sql = "INSERT INTO "+table+"  VALUES ('"+date+"','"+trans+"',"+qty+","+price+")"
+		c.execute(sql)
+		conn.commit()
+	except:
+		print ("Error: couldn't enter the values")
+if proceed == "R" or proceed == "Read" or proceed == "read":
+	Col = int(readColumns(table))
+	for row in c.execute('SELECT * FROM '+table+' ORDER BY date'):
+		for i in range(Col):
+			rowing = row[i]
+			rowing = fixWidth(rowing, columnWidth)
+		        print rowing, #
+		print(" ")
+
+if proceed == "C":
+	print(readColumns(table))
+
+if proceed == "S":
+	c.execute('PRAGMA TABLE_INFO('+table+')')
+	columns = c.fetchall()
+	print " "
+	for row in columns:
+		print "UNIX: ", fixWidth(row[0], 10), "NAME: ", fixWidth(row[1], 10), " TYPE: ", fixWidth(row[2], 10)
+	print " "
+	column =  int(raw_input("What column do you wish to query?"))
+	columnName = 0
+	if isinstance(column, int):
+		columnName = columns[column][1]
+	print " "
+	search = raw_input("what do you want to search for?")
+	print " "
+	search = "'" + str(search) + "'" 
+
+	sql = "SELECT * from " + table + " WHERE " +str(columnName) + "=" + search
+	colN = readColumns(table)
+	for row in c.execute(sql):
+		for i in range(colN):
+			print fixWidth(row[i], columnWidth), #
+		print " "
